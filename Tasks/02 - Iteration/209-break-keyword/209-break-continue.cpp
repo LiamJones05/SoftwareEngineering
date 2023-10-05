5f8cec585a1d5d4e46dd08d36e14ae70d7cc6bb1
LiamJones05 <88446020+LiamJones05@users.noreply.github.com>
2023-10-05 15:38:27 +0100

    Lab-2 complete

--- Tasks/02 - Iteration/209-break-keyword/209-break-continue.cpp
+++ Tasks/02 - Iteration/209-break-keyword/209-break-continue.cpp
@@ -6,7 +6,7 @@ int main()
     {
         char c = getchar(); //Read character
         char _ = getchar(); //Read and discard newline (nice little hack :)
-        if (c == 'q')          
+        if (c == 'q' || c == 'Q')
         {
             printf("\tUser stopped the loop early at %d\n", i);
             break;                  // break out of the for loop
